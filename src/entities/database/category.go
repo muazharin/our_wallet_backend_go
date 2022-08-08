8e05568f183faf4c4675a3478335189de35b7297
muazharin <alfanmuazharin@gmail.com>
2022-08-08 16:00:46 +0700

    update database

--- src/entities/database/category.go
+++ src/entities/database/category.go
@@ -1,8 +1,11 @@
 package database
 
 type Category struct {
-	CategoryID     int64  `gorm:"primary_key:auto_increment" json:"category_id"`
-	CategoryUserID int64  `gorm:"not null" json:"-"`
-	CategoryTitle  string `gorm:"type:varchar(100)" json:"category_title"`
-	User           Users  `gorm:"foreignkey:CategoryUserID;constraint:onUpdate:CASCADE,onDelete:CASCADE" json:"category_user_id"`
+	CategoryID       int64   `gorm:"primary_key:auto_increment" json:"category_id"`
+	CategoryUserID   int64   `gorm:"not null" json:"-"`
+	CategoryWalletID int64   `gorm:"not null" json:"-"`
+	CategoryTitle    string  `gorm:"type:varchar(100)" json:"category_title"`
+	CategoryType     string  `gorm:"type:varchar(100)" json:"category_type"`
+	User             Users   `gorm:"foreignkey:CategoryUserID;constraint:onUpdate:CASCADE,onDelete:CASCADE" json:"category_user_id"`
+	Wallet           Wallets `gorm:"foreignkey:CategoryWalletID;constraint:onUpdate:CASCADE,onDelete:CASCADE" json:"category_wallet_id"`
 }
