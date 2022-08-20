038e0b5aff7445af03498611993b34eeca9790f8
muazharin <alfanmuazharin@gmail.com>
2022-08-20 19:21:06 +0700

    remove field userId

--- src/entities/database/category.go
+++ src/entities/database/category.go
@@ -2,8 +2,8 @@ package database
 
 type Category struct {
 	CategoryID       int64  `gorm:"primary_key:auto_increment" json:"category_id"`
-	CategoryUserID   int64  `gorm:"not null" json:"category_user_id"`
 	CategoryWalletID int64  `gorm:"not null" json:"category_wallet_id"`
 	CategoryTitle    string `gorm:"type:varchar(100)" json:"category_title"`
 	CategoryType     string `gorm:"type:varchar(100)" json:"category_type"`
+	CategoryIsActive bool   `gorm:"not null" json:"category_is_active"`
 }
