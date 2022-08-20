0fbe20fca1aff42f9dc0dbd0768d130d76ea5966
muazharin <alfanmuazharin@gmail.com>
2022-08-20 19:21:51 +0700

    update field category_get_all

--- src/entities/request/category_get_all.go
+++ src/entities/request/category_get_all.go
@@ -3,6 +3,6 @@ package request
 type CategoryGetAllRequest struct {
 	CategoryType     string `json:"category_type"`
 	CategoryWalletId string `json:"category_wallet_id"`
-	CategoryUserId   string `json:"category_user_id"`
-	CategoryPage     int64  `json:"category_page"`
+	// CategoryUserId   string `json:"category_user_id"`
+	CategoryPage int64 `json:"category_page"`
 }
